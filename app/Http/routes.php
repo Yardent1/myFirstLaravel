73554c74c352e32bf354144c837a2c871e6d728e
master <qewrqw@dwfds.com>
2018-05-18 19:33:48 +0300

    form

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,25 +1,39 @@
 <?php
 
-  use App\Task;
-  use Illuminate\Http\Request;
+use App\Task;
+use Illuminate\Http\Request;
 
-  /**
-   * Вывести панель с задачами
-   */
-  Route::get('/', function () {
-      return view('tasks');
-  });
+/**
+ * Вывести панель с задачами
+ */
+Route::get('/', function () {
+    return view('tasks');
+});
 
-  /**
-   * Добавить новую задачу
-   */
-  Route::post('/task', function (Request $request) {
-    //
-  });
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255|min:6',
+    ]);
 
-  /**
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+    // Создание задачи...
+    return redirect('/');
+});
+
+
+/**
    * Удалить задачу
    */
-  Route::delete('/task/{task}', function (Task $task) {
+ // Route::delete('/task/{task}', function (Task $task) {
     //
-  });
+//  });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
   <head>
     <title>Laravel Quickstart - Basic</title>
-
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <!-- CSS и JavaScript -->
   </head>
 
